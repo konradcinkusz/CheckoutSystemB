1389676c323fed1a9d641281c94f3db02704aa17
Konrad Cinkusz <konradcinkusz@gmail.com>
2022-02-24 23:21:05 +0100

    additional test

--- CheckoutSystemB.Tests/Services/CheckoutServiceTests.cs
+++ CheckoutSystemB.Tests/Services/CheckoutServiceTests.cs
@@ -28,6 +28,8 @@ namespace CheckoutSystemB.Services.Tests
             Assert.IsFalse(applied);
         }
 
+        const decimal TestCase1_expected_result = 108.07M;
+
         [TestMethod()]
         public void TestCase1()
         {
@@ -46,7 +48,37 @@ namespace CheckoutSystemB.Services.Tests
             var result = checkoutService.Total();
             //Test
             Assert.IsTrue(decimal.TryParse(result.Split('£')[1], out decimal resultPrice));
-            Assert.AreEqual(108.07M, Math.Round(resultPrice, 2));
+            Assert.AreEqual(TestCase1_expected_result, Math.Round(resultPrice, 2));
+        }
+
+        /// <summary>
+        /// We expect the same result as in TestCase1
+        /// With this test we can see that the analytic document isn’t well enough because there is no information 
+        /// about promotion priority, and we should (nice to have) implement some logic of promotions ordering, 
+        /// which promotions should be applied as first. With this solution ordering is resolved by just adding 
+        /// promotions in the right order. I implement better solution that in my 
+        /// lack-of-tests-unstructured .NET 6 C# 10 solution.
+        /// https://github.com/konradcinkusz/CheckoutSystem/blob/feature/promotions_ordering/CheckoutSystem/Program.cs
+        /// </summary>
+        [TestMethod()]
+        public void TestCase1_a_failed()
+        {
+            //Arrange
+            ICheckoutService checkoutService = new CheckoutService();
+            var promotions = ImmutableArray.Create(new Promotion[] { new BottlePriceDrop(0001), new Discount() });
+            checkoutService.Scan(new List<Product>()
+            {
+                new Product{Id = 0001, Name= "Watter Bottle", Price = 24.95M},
+                new Product{Id = 0001, Name= "Watter Bottle", Price = 24.95M},
+                new Product{Id = 0002, Name= "Hoodie", Price = 65.00M},
+                new Product{Id = 0003, Name= "Sticker Set", Price =  3.99M},
+            });
+            checkoutService.ApplyPromotions(promotions);
+            //Act
+            var result = checkoutService.Total();
+            //Test
+            Assert.IsTrue(decimal.TryParse(result.Split('£')[1], out decimal resultPrice));
+            Assert.AreEqual(TestCase1_expected_result, Math.Round(resultPrice, 2));
         }
 
         [TestMethod()]
