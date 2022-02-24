5d004c77bdae68f8b83055220dd18aeb993cb944
Konrad Cinkusz <konradcinkusz@gmail.com>
2022-02-24 23:10:34 +0100

    comment

--- CheckoutSystemB/BLL/Services/CheckoutService.cs
+++ CheckoutSystemB/BLL/Services/CheckoutService.cs
@@ -8,7 +8,9 @@ namespace CheckoutSystemB.BLL.Services
 {
     public class CheckoutService : ICheckoutService
     {
-        //Eventually, if we don't plan to expand Checkout classes we can use List<Product> directly
+        //I know, it's not so clean, but you mention 'Do not worry about the storage or retrieval of any items in a database'.
+        //So I store checkout object here, and checkout service logic is implemented as follow due to that fact.
+        //Eventually, if we don't plan to expand Checkout classes we can use List<Product> directly.
         private Checkout checkout { get; set; } = null;
         public bool Scan(List<Product> products)
         {
